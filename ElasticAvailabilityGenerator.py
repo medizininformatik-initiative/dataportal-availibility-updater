468183375f311a154d1321b7b69709b08042549c
Julian Gründner <juliangruendner@googlemail.com>
2025-01-15 17:13:33 +0100

    initialise availability generator

---
+++ ElasticAvailabilityGenerator.py
@@ -0,0 +1,155 @@
+import json
+import logging
+import os
+import uuid
+
+
+class ElasticAvailabilityGenerator:
+    namespace_uuid_str = '00000000-0000-0000-0000-000000000000'
+    extension = '.json'
+    max_filesize_mb = 10
+
+    def __init__(self, availability_input_dir, availability_output_dir, es_ontology_dir):
+        self.availability_input_dir = availability_input_dir
+        self.availability_output_dir = availability_output_dir
+        self.es_ontology_dir = es_ontology_dir
+        self.es_onto_tree = {}
+
+        with open(os.path.join(self.availability_input_dir, 'stratum-to-context.json')) as fp:
+            self.stratum_to_context = json.load(fp)
+
+    def __get_contextualized_termcode_hash(self, context_node: dict, termcode_node: dict):
+
+        context_termcode_hash_input = f"{context_node.get('system')}{context_node.get('code')}{context_node.get('version', '')}{termcode_node.get('system')}{termcode_node.get('code')}"
+
+        namespace_uuid = uuid.UUID(self.namespace_uuid_str)
+        return str(uuid.uuid3(namespace_uuid, context_termcode_hash_input))
+
+    def get_avail_sum_for_all_children(self, parent_id):
+
+        count = self.es_onto_tree[parent_id]["availability"]
+
+        for child in self.es_onto_tree[parent_id]["children"]:
+            count = count + self.get_avail_sum_for_all_children(child["contextualized_termcode_hash"])
+
+        return count
+
+    def convert_measure_score_to_ranges(self, measure_score):
+        buckets = [0, 10, 100, 1000, 10000, 50000, 100000, 150000, 200000, 1000000]
+        return max(b for b in buckets if measure_score >= b)
+
+    def get_hashed_tree(self):
+
+        directory = os.path.join(self.es_ontology_dir, "elastic")
+
+        for filename in os.listdir(directory):
+            if 'onto_es__ontology' in filename:
+                filepath = os.path.join(directory, filename)
+
+                logging.debug("Opening file: " + filepath)
+
+                with open(filepath, 'r') as file:
+                    for line in file:
+                        line = line.strip()
+
+                        if line:
+                            obj = json.loads(line)
+
+                            if "index" in obj:
+                                cur_hash = obj["index"]["_id"]
+                            else:
+
+                                self.es_onto_tree[cur_hash] = {
+                                    "availability": 0,
+                                    "children": obj["children"]
+                                }
+
+    def update_availability_on_hash_tree(self):
+
+        hash_set = set()
+
+        for filename in os.listdir(self.availability_input_dir):
+            if 'availability_report' in filename:
+                filepath = os.path.join(self.availability_input_dir, filename)
+
+                with open(filepath, "r") as f:
+                    report = json.load(f)
+
+                    for group in report["group"]:
+
+                        for stratifier in group["stratifier"]:
+                            if "stratum" in stratifier:
+
+                                strat_code = stratifier["code"][0]["coding"][0]["code"]
+
+                                if strat_code not in self.stratum_to_context:
+                                    continue
+
+                                context = self.stratum_to_context[strat_code]
+
+                                for stratum in stratifier["stratum"]:
+                                    measure_score = stratum["measureScore"]["value"]
+
+                                    if "system" not in stratum["value"]["coding"][0]:
+                                        continue
+
+                                    strat_system = stratum["value"]["coding"][0]["system"]
+                                    strat_code = stratum["value"]["coding"][0]["code"]
+
+                                    termcode = {
+                                        "system": strat_system,
+                                        "code": strat_code
+                                    }
+
+                                    if context:
+                                        hash = self.__get_contextualized_termcode_hash(context, termcode)
+
+                                        hash_set.add(hash)
+                                        if hash in self.es_onto_tree:
+                                            self.es_onto_tree[hash]["availability"] = self.es_onto_tree[hash][
+                                                                                          "availability"] + measure_score
+
+    def __write_es_to_file(self, es_availability_inserts, max_filesize_mb, filename_prefix, extension, write_dir):
+
+        current_file_subindex = 1
+        current_file_size = 0
+        count = 0
+
+        current_file_name = os.path.join(write_dir, f"{filename_prefix}_{current_file_subindex}{extension}")
+        with open(current_file_name, 'w+', encoding='UTF-8') as current_file:
+
+            for insert in es_availability_inserts:
+
+                count = count + 1
+                current_line = f"{json.dumps(insert, ensure_ascii=False)}\n"
+                current_file.write(current_line)
+                current_file_size += len(current_line)
+
+                if current_file_size > max_filesize_mb * 1024 * 1024 and count % 2 == 0:
+                    current_file_subindex += 1
+                    current_file_name = os.path.join(write_dir, f"{filename_prefix}_{current_file_subindex}{extension}")
+                    current_file_size = 0
+                    current_file.close()
+                    current_file = open(current_file_name, 'w', encoding='UTF-8')
+
+    def generate_availability(self):
+
+        es_availability_inserts = []
+        self.get_hashed_tree()
+        self.update_availability_on_hash_tree()
+
+        for key, value in self.es_onto_tree.items():
+            sum_all_children = self.get_avail_sum_for_all_children(key)
+            insert_hash = {"update": {"_id": key}}
+            insert_availability = {
+                "doc": {"availability": self.convert_measure_score_to_ranges(sum_all_children)}}
+
+            if sum_all_children > 0:
+                logging.debug(
+                    f'key {key} is available with {sum_all_children} - converted to {self.convert_measure_score_to_ranges(sum_all_children)}')
+
+            es_availability_inserts.append(insert_hash)
+            es_availability_inserts.append(insert_availability)
+
+        self.__write_es_to_file(es_availability_inserts, self.max_filesize_mb,
+                                "es_availability_update", self.extension, self.availability_output_dir)
---
+++ generate-availability.py
@@ -0,0 +1,110 @@
+import argparse
+import json
+import logging
+import os
+import zipfile
+
+import requests
+
+from ElasticAvailabilityGenerator import ElasticAvailabilityGenerator
+
+
+def download_and_unzip(url, extract_to):
+    local_zip_path = "temp.zip"
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()  # Raise an error for bad status codes
+        with open(local_zip_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+    os.makedirs(extract_to, exist_ok=True)
+    with zipfile.ZipFile(local_zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_to)
+
+    os.remove(local_zip_path)
+
+
+def download_availability_reports(availability_input_dir, fhir_base_url):
+    response = requests.get(f'{fhir_base_url}/DocumentReference?_count=500&_format=json')
+
+    doc_refs = response.json()
+
+    for doc_ref in doc_refs['entry']:
+
+        doc_ref = doc_ref['resource']
+
+        author = doc_ref['author'][0]['identifier']['value']
+        master_ident_system = doc_ref['masterIdentifier']['system']
+        master_ident_value = doc_ref['masterIdentifier']['value']
+
+        if not (master_ident_system == 'http://medizininformatik-initiative.de/sid/project-identifier'
+                and master_ident_value == 'fdpg-availability-report'):
+            logging.info(f'Not a availability report Doc ref -> skipping')
+            continue
+
+        if len(doc_ref.get('content', [])) != 1:
+            logging.warning(f'Found measure reference not equal to 1 - expected measure ref number equal to 1')
+            continue
+
+        measure_ref = doc_ref.get('content')[0].get('attachment', {}).get('url', None)
+
+        if measure_ref is None:
+            logging.warning(
+                f'Could not find a Measure Reference for availability report Doc Reference - ignoring Doc Reference')
+            continue
+
+        response = requests.get(f'{fhir_base_url}/{measure_ref}?_format=json')
+
+        measure_report = response.json()
+
+        report_file_name = f'availability_report_{author}.json'
+
+        with open(os.path.join(availability_input_dir, report_file_name), 'w+') as fp:
+            json.dump(measure_report, fp)
+
+
+def update_availability_in_es(es_base_url, es_index, availability_dir):
+    es_url = f'{es_base_url}/{es_index}/_bulk'
+
+    for filename in os.listdir(availability_dir):
+        file_path = os.path.join(availability_dir, filename)
+        if os.path.isfile(file_path):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                response = requests.post(
+                    es_url,
+                    headers={'Content-Type': 'application/json'},
+                    data=file
+                )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--onto_repo', type=str)
+    parser.add_argument('--onto_git_tag', type=str)
+    parser.add_argument('--ontology_dir', type=str)
+    parser.add_argument('--availability_input_dir', type=str)
+    parser.add_argument('--availability_output_dir', type=str)
+    parser.add_argument('--availability_report_server_base_url', type=str)
+    parser.add_argument('--es_base_url', type=str)
+    parser.add_argument('--es_index', type=str)
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    os.makedirs(args.ontology_dir, exist_ok=True)
+    os.makedirs(args.availability_input_dir, exist_ok=True)
+    os.makedirs(args.availability_output_dir, exist_ok=True)
+
+    if not os.path.isdir(os.path.join(args.ontology_dir, "elastic")):
+        onto_download_url = f'{args.onto_repo}/{args.onto_git_tag}/elastic.zip'
+        download_and_unzip(onto_download_url, args.ontology_dir)
+
+    download_availability_reports(args.availability_input_dir, args.availability_report_server_base_url)
+
+    es_avail_generator = ElasticAvailabilityGenerator(args.availability_input_dir, args.availability_output_dir,
+                                                      args.ontology_dir)
+    es_avail_generator.generate_availability()
+    update_availability_in_es(args.es_base_url, args.es_index, args.availability_output_dir)
